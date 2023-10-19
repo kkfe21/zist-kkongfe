1ba536eda61253977d9ec37161663b7702002e75
kkfe21 <khyew21@gmail.com>
2023-10-19 11:35:18 +0900

    add settings

--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSeriallizer
+from .models import User
 
 # Create your views here
 def index(request):
 	return render(request, "main/index.html")
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
